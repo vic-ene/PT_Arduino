8fa92752f33eeed48fa721b0cd6d93626e7882dd
vic-ene <vicenescu67@gmail.com>
2021-03-29 13:47:59 +0200

    Sharing github

--- arduino_code.ino
+++ arduino_code.ino
@@ -22,29 +22,39 @@ void loop() {
   sensors_event_t event; 
   mma.getEvent(&event);
 
-  String time = "," + String(millis());
+  String time =  String(millis());
 
 
  // Serial.print("-1,");
   
   // Temperature 
-  Serial.print("0,0," + String(analogRead(2))  + time + ",");
-  
+  //Serial.print("0,0," + String(analogRead(2))  + time + ",");
+  printValue("0", "0", analogRead(2), time, true);
 
   // Strain gauge
-  Serial.print("1,0," + String(analogRead(0)) + time + ",");
-  Serial.print("1,1," + String(analogRead(1)) + time + ",");
-
+  //Serial.print("1,0," + String(analogRead(0)) + time + ",");
+  //Serial.print("1,1," + String(analogRead(1)) + time + ",");
+   printValue("1", "0", analogRead(0), time, true);
+   printValue("1", "0", analogRead(1), time, true);
+  
 
   // Accelerometer
-  Serial.print("2,0," + String(event.acceleration.x) + time + ","); 
-  Serial.print("2,1," + String(event.acceleration.y) + time + ","); 
-  Serial.print("2,2," +String(event.acceleration.z) + time); 
+  //Serial.print("2,0," + String(event.acceleration.x) + time + ","); 
+  //Serial.print("2,1," + String(event.acceleration.y) + time + ","); 
+  //Serial.print("2,2," +String(event.acceleration.z) + time); 
+  printValue("2", "0", event.acceleration.x, time, true);
+  printValue("2", "1", event.acceleration.y, time, true);
+  printValue("2", "2", event.acceleration.z, time, false);
   
 
   
   Serial.println();
 
   delay(500);
+
   
+}
+
+void printValue(String plot, String layer, float value, String time, boolean coma){
+  Serial.print(plot + "," + layer + ","  + String(value) + "," + String(time) + ((coma == true) ? "," : ""));
 }
