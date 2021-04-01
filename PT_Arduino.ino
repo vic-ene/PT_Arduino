e01ad799f9b0a8a064a4dc0cc2a7b039cd2838cb
vic-ene <vicenescu67@gmail.com>
2021-04-01 15:14:16 +0200

    Added error message to accelerometer
    Don't know how to do for the analog pins

--- PT_Arduino.ino
+++ PT_Arduino.ino
@@ -3,51 +3,48 @@
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
 
+
+
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
 void setup(void) {
   Serial.begin(9600);
+
+   
+  if (! mma.begin()) {
+    Serial.println("Accelerometer Not working");
+  }
+  mma.setRange(MMA8451_RANGE_8_G);
+
+  
   
 }
 
 void loop() {
-  
-  if (! mma.begin()) {
-  
-  }
-  
-  mma.setRange(MMA8451_RANGE_8_G);
+ 
+  // read values from the accelerometer
   mma.read();
-  
   sensors_event_t event; 
   mma.getEvent(&event);
 
+
+  // find the current time and convert it into a string
   String time =  String(millis());
 
-
- // Serial.print("-1,");
   
-  // Temperature 
-  //Serial.print("0,0," + String(analogRead(2))  + time + ",");
-  printValue("0", "0", analogRead(2), time, true);
+   // Temperature 
+   printValue("0", "0", analogRead(2), time, true);
 
-  // Strain gauge
-  //Serial.print("1,0," + String(analogRead(0)) + time + ",");
-  //Serial.print("1,1," + String(analogRead(1)) + time + ",");
+   // Strain gauge
    printValue("1", "0", analogRead(0), time, true);
    printValue("1", "0", analogRead(1), time, true);
   
-
-  // Accelerometer
-  //Serial.print("2,0," + String(event.acceleration.x) + time + ","); 
-  //Serial.print("2,1," + String(event.acceleration.y) + time + ","); 
-  //Serial.print("2,2," +String(event.acceleration.z) + time); 
-  printValue("2", "0", event.acceleration.x, time, true);
-  printValue("2", "1", event.acceleration.y, time, true);
-  printValue("2", "2", event.acceleration.z, time, false);
+   // Accelerometer
+   printValue("2", "0", event.acceleration.x, time, true);
+   printValue("2", "1", event.acceleration.y, time, true);
+   printValue("2", "2", event.acceleration.z, time, false);
   
 
-  
   Serial.println();
 
   delay(500);
